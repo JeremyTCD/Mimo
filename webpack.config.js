adf66229e608d30a713c7abcc36eb9e783f150e5
JeremyTCD <jeremytanchongdao@gmail.com>
2017-02-17 23:04:31 +0800

    Updated webpack.config.js
    - Webpack builds the template

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-    entry: './scripts/index.ts',
+    entry: {bundle: path.join(__dirname, '/scripts/index.ts'),
+        searchWorker: path.join(__dirname, '/scripts/workers/search.worker.ts')},
     output: {
-        filename: 'bundle.js',
-        path: "./bin/styles"
+        filename: '[name].js',
+        path: path.join(__dirname, '/bin/styles')
     },
     resolve: {
-        extensions: ['.ts', '.js']
+        extensions: ['.ts', '.js'],
+        modules: [path.join(__dirname, '/node_modules')]
+    },
+    resolveLoader:{
+        modules: [path.join(__dirname, '/node_modules')]
     },
     module: {
         rules: [
@@ -17,25 +23,27 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
-                    use: [{ loader: "css-loader" },
-                    { loader: "sass-loader" }]
+                    use: [{ loader: 'css-loader' },
+                    { loader: 'sass-loader' }]
                 })
             }
         ]
     },
     plugins: [
         new ExtractTextPlugin({
-            filename: "bundle.css"
+            filename: 'bundle.css'
         }),
         new CopyWebpackPlugin([
-            { from: './fonts', to: '../fonts' },
-            { from: './misc', to: '../misc'},
-            { from: './templates', to: '../'},
-            { from: './common.js', to: '../common.js'}
+            { from: path.join(__dirname, '/fonts'), to: '../fonts' },
+            { from: path.join(__dirname, '/misc'), to: '../'},
+            { from: path.join(__dirname, '/templates'), to: '../'},
+            { from: path.join(__dirname, '/docfx.js'), to: '../common.js'},
+            { from: path.join(__dirname, '/plugins'), to: '../plugins'}
         ]),
         new webpack.ProvidePlugin({
             $:'jquery',
-            'window.jQuery': 'jquery'
+            'window.jQuery': 'jquery',
+            jQuery: 'jquery'
         })
     ]
 };
