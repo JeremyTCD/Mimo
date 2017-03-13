3107f1f09495ad497769d753b5b8d564661aabca
JeremyTCD <jeremytanchongdao@gmail.com>
2017-03-13 15:39:44 +0800

    Added Component abstract class

---
+++ scripts/component.ts
@@ -0,0 +1,13 @@
+﻿export abstract class Component {
+    public initialize(): void {
+        if (this.canInitialize()) {
+            this.setup();
+            this.registerListeners();
+        }
+    }
+
+    protected abstract canInitialize(): boolean;
+    protected abstract setup(): void;
+    protected abstract registerListeners(): void;
+}
+
