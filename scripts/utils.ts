1850c8fb9c252f89dbb4e8216aad91b21db8f093
JeremyTCD <jeremytanchongdao@gmail.com>
2017-03-13 15:54:27 +0800

    Added mediaWidthNarrow utility function
    - Should eventually be used to cache values

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -125,6 +125,10 @@ export function htmlDecode(value: string): string {
         .replace(/&amp;/g, '&');
 }
 
+export function mediaWidthNarrow(): boolean {
+    return window.matchMedia('(max-width: 768px)').matches;
+}
+
 export function mediaWidthWide(): boolean {
     return window.matchMedia('(min-width: 1025px)').matches;
 }
