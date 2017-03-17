896271113e6ba5abcc2a96cba69a5009f74325c7
JeremyTCD <jeremytanchongdao@gmail.com>
2017-03-17 19:11:14 +0800

    build.js now executes msbuild
    - msbuild restores packages and copies required dlls to plugins

--- tools/build.js
+++ tools/build.js
@@ -1,9 +1,13 @@
-const copySimpleFilesToBin = require('./build.copy');
+const copySimpleFilesToBin = require('./copySimpleFilesToBin');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 const rimraf = require('rimraf');
+const execSync = require('child_process').execSync;
+const path = require('path');
 
 rimraf.sync('./bin');
+
+execSync('msbuild', { stdio: [0, 1, 2] });
 
 copySimpleFilesToBin();
 
