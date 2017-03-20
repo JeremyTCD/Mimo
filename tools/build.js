4cb5018fc4a9d5161640bdd60e7eb1a6c095f750
JeremyTCD <jeremytanchongdao@gmail.com>
2017-03-20 20:57:14 +0800

    Minor comment formatting for build.js

--- tools/build.js
+++ tools/build.js
@@ -4,22 +4,23 @@ const webpackCompile = require('./webpackCompile');
 const clean = require('./clean');
 
 async function build() {
-    console.log('*** start - clean ***');
+    console.log(`*** start - release build ***`);
+    console.log('start - clean');
     await clean();
-    console.log('*** complete - clean ***');
+    console.log('complete - clean');
 
-    console.log('*** start - restore plugins ***');
+    console.log('start - restore plugins');
     await restorePlugins('debug');
-    console.log('*** complete - restore plugins ***');
+    console.log('complete - restore plugins');
 
-    console.log('*** start - copy simple files ***');
+    console.log('start - copy simple files');
     await copySimpleFilesToDist();
-    console.log('*** complete - copy simple files ***');
+    console.log('complete - copy simple files');
 
-    console.log('*** start - webpack compile ***');
+    console.log('start - webpack compile');
     await webpackCompile();
-    console.log('*** complete - webpack compile ***');
-
+    console.log('complete - webpack compile');
+    console.log(`*** complete - release build ***`);
 }
 
 build();
