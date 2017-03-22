6e36c3d769e70b967d23fdbcd29b143a2d7eafdc
JeremyTCD <jeremytanchongdao@gmail.com>
2017-03-22 21:05:25 +0800

    MsBuild script now removes DocAsCode assemblies and copies assemblies directly to plugins

--- tools/copySimpleFilesToDist.js
+++ tools/copySimpleFilesToDist.js
@@ -4,9 +4,9 @@ const cpy = require('cpy');
 module.exports = function copySimpleFilesTodist() {
     const outputDir = path.join(__dirname, '../dist');
 
-    return Promise.all([cpy(path.join(__dirname, '../fonts/*.*'), path.join(outputDir, 'fonts')),
-    cpy([path.join(__dirname, '../plugins/net452/*.*'), '!**/Microsoft.DocAsCode.*.dll'], path.join(outputDir, 'plugins')),
-    cpy(path.join(__dirname, '../templates/*.*'), outputDir),
-    cpy(path.join(__dirname, '../templates/partials/*.*'), path.join(outputDir, 'partials')),
-    cpy(path.join(__dirname, '../misc/*.*'), outputDir)]);
+    return Promise.all([cpy(path.join(__dirname, '../fonts/*'), path.join(outputDir, 'fonts')),
+    cpy(path.join(__dirname, '../plugins/*'), path.join(outputDir, 'plugins')),
+    cpy(path.join(__dirname, '../templates/*'), outputDir),
+    cpy(path.join(__dirname, '../templates/partials/*'), path.join(outputDir, 'partials')),
+    cpy(path.join(__dirname, '../misc/*'), outputDir)]);
 };
