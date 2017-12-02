c9e3db8582739229c73e7524ef59644f587ba3a2
JeremyTCD <jeremytanchongdao@gmail.com>
2017-12-02 21:22:22 +0800

    Updated search.worker.ts to use new lunr version

--- scripts/workers/search.worker.ts
+++ scripts/workers/search.worker.ts
@@ -1,12 +1,14 @@
-(function() {
-  let lunr = require('lunr');
-  let lunrIndex = lunr(function() {
-      this.pipeline.remove(lunr.stopWordFilter);
-      this.ref('relPath');
-      this.field('title', { boost: 50 });
-      this.field('text', { boost: 20 });
-  });
-  lunr.tokenizer.seperator = /[\s\-\.]+/;
+import * as lunr from 'lunr';
+
+(function () {
+  let lunrBuilder = new lunr.Builder();
+  lunrBuilder.pipeline.remove(lunr.stopWordFilter);
+  lunrBuilder.ref('relPath');
+  lunrBuilder.field('title');
+  lunrBuilder.field('text');
+
+  // TODO - https://github.com/olivernn/lunr.js/issues/323
+  // lunr.tokenizer.separator = /[\s\-\.]+/;
 
   let stopWordsRequest = new XMLHttpRequest();
   stopWordsRequest.open('GET', '/search-stopwords.json');
@@ -17,7 +19,7 @@
     let stopWords = JSON.parse(stopWordsRequest.responseText);
     let docfxStopWordFilter = lunr.generateStopWordFilter(stopWords);
     lunr.Pipeline.registerFunction(docfxStopWordFilter, 'docfxStopWordFilter');
-    lunrIndex.pipeline.add(docfxStopWordFilter);
+    lunrBuilder.pipeline.add(docfxStopWordFilter);
   }
   stopWordsRequest.send();
 
@@ -25,26 +27,42 @@
   let searchDataRequest = new XMLHttpRequest();
 
   searchDataRequest.open('GET', '/index.json');
-  searchDataRequest.onload = function() {
+  searchDataRequest.onload = function () {
     if (searchDataRequest.status != 200) {
       return;
     }
     searchData = JSON.parse(searchDataRequest.responseText);
     for (let prop in searchData) {
-      lunrIndex.add(searchData[prop]);
+      lunrBuilder.add(searchData[prop]);
     }
-    postMessage({e: 'index-ready'});
+    postMessage({ e: 'index-ready' });
   }
   searchDataRequest.send();
 
+  let lunrIndex: lunr.Index;
   onmessage = function (event: MessageEvent) {
-    let q = event.data.q;
-    let hits = lunrIndex.search(q);
+    if (!lunrIndex) {
+      lunrIndex = lunrBuilder.build();
+    }
+
+    let queryString = event.data.q;
+    let hits = lunrIndex.query(q => {
+      q.term(queryString, { fields:['title'], usePipeline: true, boost: 200 })
+      // Start and end wild cards, e.g "correct" will match with "rre". Note that wildcard query strings are not passed 
+      // through the search pipeline since they are hard to stem.
+      q.term("*" + queryString + "*", {  fields:['title'], usePipeline: false, boost: 100 }) 
+      // Fuzzy maching, e.g "correct" will match with search term "correcc"
+      q.term(queryString, {  fields:['title'], usePipeline: false, editDistance: 1 }) 
+      
+      q.term(queryString, { fields:['text'], usePipeline: true, boost: 100 })
+      q.term("*" + queryString + "*", {  fields:['text'], usePipeline: false, boost: 10 }) 
+      q.term(queryString, {  fields:['text'], usePipeline: false, editDistance: 1 }) 
+    });
     let results = [];
-    hits.forEach(function(hit) {
+    hits.forEach(function (hit) {
       let item = searchData[hit.ref];
       results.push(item.snippetHtml);
     });
-    postMessage({e: 'query-ready', q: q, d: results});
+    postMessage({ e: 'query-ready', q: queryString, d: results });
   }
 })();
