27f309c9f06b9462690b33b2754123644609c380
JeremyTCD <jeremytanchongdao@gmail.com>
2017-12-02 21:27:54 +0800

    Minor change to copySimpleFilesToDist to avoid attempting to copy directories

--- tools/copySimpleFilesToDist.js
+++ tools/copySimpleFilesToDist.js
@@ -6,7 +6,7 @@ module.exports = function copySimpleFilesTodist() {
 
     return Promise.all([cpy(path.join(__dirname, '../fonts/*'), path.join(outputDir, 'fonts')),
     cpy(path.join(__dirname, '../plugins/*'), path.join(outputDir, 'plugins')),
-    cpy(path.join(__dirname, '../templates/*'), outputDir),
+    cpy(path.join(__dirname, '../templates/*'), outputDir, {nodir: true}), // Attempting to copy a dir causes errors
     cpy(path.join(__dirname, '../templates/partials/*'), path.join(outputDir, 'partials')),
     cpy(path.join(__dirname, '../misc/*'), outputDir)]);
 };
