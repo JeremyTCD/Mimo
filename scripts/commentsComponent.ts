e7401e592f00cd8136be4dd7bc22c4dce399a3ac
JeremyTCD <jeremytanchongdao@gmail.com>
2017-12-19 15:27:47 +0800

    Added comments component

--- scripts/commentsComponent.ts
+++ scripts/commentsComponent.ts
@@ -1,4 +1,5 @@
 import Component from './component';
+import { currentHeightToAutoHeight, autoHeightToFixedHeight } from './transitionsService';
 
 class CommentsComponent extends Component {
     disqusThread: HTMLElement;
@@ -28,35 +29,59 @@ class CommentsComponent extends Component {
     }
 
     protected setup(): void {
-                
     }
 
     protected registerListeners(): void {
-        window.addEventListener('scroll', this.checkLoadRequired);
+        if (!this.tryLoad()) {
+            window.addEventListener('scroll', this.tryLoad);
+        }
     }
 
-    private checkLoadRequired = (): void => {
+    private tryLoad = (): boolean => {
         // top is relative to the top of the viewport, so as you scroll down, this number decreases
         let disqusTop: number = this.disqusThread.getBoundingClientRect().top;
 
         if (disqusTop < (window.innerHeight || document.documentElement.clientHeight)) {
-            window.removeEventListener('scroll', this.checkLoadRequired);
+            window.removeEventListener('scroll', this.tryLoad);
 
             let component = this;
 
-            window['disqus_config'] = function() {
-                this.page.url = location.host + location.pathname;
+            window['disqus_config'] = function () {
+                this.page.url = `https://${location.host}${location.pathname}`;
                 this.page.identifier = component.disqusIdentifier;
+                this.callbacks.onReady.push(component.disqusOnReady);
             };
 
             let disqusScript: HTMLScriptElement = document.createElement('script');
             disqusScript.src = `https://${this.disqusShortname}.disqus.com/embed.js`;
-            disqusScript.setAttribute('data-timestamp', Date());
+            disqusScript.setAttribute('data-timestamp', Date.now().toString());
 
             (document.head || document.body).appendChild(disqusScript);
 
             console.log('Disqus embed script inserted.');
+
+            return true;
         }
+
+        return false;
+    }
+
+    private disqusOnReady = (): void => {
+        let commentsLoader: HTMLElement = document.getElementById('comments-loader');
+
+        commentsLoader.style.opacity = '0';
+        autoHeightToFixedHeight(commentsLoader, 0);
+        commentsLoader.addEventListener('transitionend', this.commentsLoaderOnRemoved);
+
+        // Disqus has some unusual behaviour whereby it it displays a login menu for a split 
+        // second even if you are logged in (after the onready callback is called). This causes
+        // an unavoidable jerk.
+        currentHeightToAutoHeight(this.disqusThread);
+    }
+
+    private commentsLoaderOnRemoved = (event: Event): void => {
+        event.target.removeEventListener('transitionend', this.commentsLoaderOnRemoved);
+        (event.target as HTMLElement).style.display = 'none';
     }
 }
 
