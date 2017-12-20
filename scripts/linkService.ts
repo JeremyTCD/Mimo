3b36ff5f822965e87b147ddd640b91670424d301
JeremyTCD <jeremytanchongdao@gmail.com>
2017-12-20 21:21:05 +0800

    Made opening of external links in a new tab the default. Also fixed a security vulnerability.

--- scripts/linkService.ts
+++ scripts/linkService.ts
@@ -2,9 +2,11 @@
  * Opens external links in new tabs
  */
 export function openExternalLinksInNewTab(): void {
-    if ($("meta[property='docfx:newtab']").attr("content") === "true") {
-        $(document.links).filter(function () {
+    $(document.links).
+        filter(function () {
             return (this as HTMLAnchorElement).hostname !== window.location.hostname;
-        }).attr('target', '_blank');
-    }
+        }).
+        attr('target', '_blank').
+        // Prevents malicious sites from manipulating the window object https://mathiasbynens.github.io/rel-noopener/#hax
+        attr('rel', 'noopener'); 
 }
