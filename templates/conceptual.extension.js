c4b9994546f6c6c3260bf694148c25835568713a
JeremyTCD <jeremytanchongdao@gmail.com>
2017-12-27 18:15:03 +0800

    Removed extraneous comments
    - Check fair use

--- templates/conceptual.extension.js
+++ templates/conceptual.extension.js
@@ -1,5 +1,3 @@
-// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 /**
  * This method will be called at the start of exports.transform in conceptual.html.primary.js
  */
