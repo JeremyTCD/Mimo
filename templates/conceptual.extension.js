f5e6a8ca5133512760e5326f700a79d5430affee
JeremyTCD <jeremytanchongdao@gmail.com>
2018-01-08 23:09:06 +0800

    Updated footer so that if social media icons aren't displayed, contents are still spaced evenly

--- templates/conceptual.extension.js
+++ templates/conceptual.extension.js
@@ -2,6 +2,9 @@
  * This method will be called at the start of exports.transform in conceptual.html.primary.js
  */
 exports.preTransform = function (model) {
+    model['jtcd.displaySocialMediaLinks'] = model['jtcd.githubLink'] || model['jtcd.twitterLink'] ||
+        model['jtcd.instagramLink'] || model['jtcd.facebookLink'] || model['jtcd.googleplusLink'];
+
   return model;
 }
 
