ba2883cb3c6660d7eaa60ebd66b8d374b5c41ac4
JeremyTCD <jeremytanchongdao@gmail.com>
2018-03-21 21:53:50 +0800

    Minor script reorganization

---
+++ scripts/footer/footerComponent.ts
@@ -0,0 +1,55 @@
+import Component from './component';
+import ResizeObserver from 'resize-observer-polyfill';
+import debounceService from './debounceService';
+
+class FooterComponent extends Component {
+    footerButtonElement: HTMLElement;
+    debounceTime: number = 150;
+
+    protected validDomElementExists(): boolean {
+        // Footer always exists
+        return true;
+    }
+
+    protected setupOnDomContentLoaded(): void {
+        this.footerButtonElement = document.getElementById('footer-button');
+
+        // ResizeObserver typically makes an initial call to setBackToTopButtonOpacity because other js scripts modify dom elements in body when page is loading,
+        // not reliable though.
+        this.setBackToTopButtonOpacity();
+    }
+
+    protected setupOnLoad(): void {
+        // Do nothing
+    }
+
+    protected registerListeners(): void {
+        // A selling point of ResizeObserver is that it provides dimensions so layouts can be avoided - https://developers.google.com/web/updates/2016/10/resizeobserver
+        // Consider utilizing those values.
+        const resizeObserver = new ResizeObserver(debounceService.createTimeoutDebounceFunction(this.setBackToTopButtonOpacity, this.debounceTime));
+
+        resizeObserver.observe(document.body);
+    }
+
+    private setBackToTopButtonOpacity = (): void => {
+        if (this.footerButtonElement) {
+            let visible = this.footerButtonElement.classList.contains('visible');
+
+            // This is a hack for getting around android chrome and ios safari url bars.
+            // 100vh corresponds to the viewport height + the url bar height on both and is fixed - https://developers.google.com/web/updates/2016/12/url-bar-resizing.
+            // If body.offsetHeight is === (viewport height + url bar height), there will be no verical scrollbar since url bar just scrolls out. 
+            // 100vh is just viewport height (window.innerHeight) on normal browsers, so this is safe for them.
+            let documentElementStyle = getComputedStyle(document.documentElement);
+            let viewportHeight = parseFloat(documentElementStyle.height);
+            let pageScrollable = document.body.offsetHeight > viewportHeight;
+
+            if (!visible && pageScrollable) {
+                this.footerButtonElement.classList.add('visible');
+            } else if (visible && !pageScrollable) {
+                this.footerButtonElement.classList.remove('visible');
+            }
+        }
+    }
+}
+
+export default new FooterComponent();
---
+++ scripts/main/articleComponent.ts
@@ -0,0 +1,50 @@
+﻿
+import { injectable, inject } from 'inversify';
+import INTERFACES from './INTERFACES';
+import IComponent from './IComponent';
+import ITooltipService from './ITooltipService';
+// create wrapper
+import * as Clipboard from 'clipboard';
+
+@injectable()
+export default class ArticleComponent implements IComponent {
+    @inject(INTERFACES.ITooltipService)
+    private _tooltipService: ITooltipService;
+
+    public setupOnLoad(): void {
+        // Do nothing
+    }
+
+    public validDomElementExists(): boolean {
+        // Article always exists
+        return true;
+    }
+
+    public setupOnDomContentLoaded(): void {
+        this.addLinks();
+    }
+
+    public registerListeners(): void {
+        // Do nothing
+    }
+
+    private addLinks(): void {
+        let articleElement: HTMLElement = document.querySelector('.jtcd-article') as HTMLElement;
+        let headingElements: NodeList = articleElement.querySelectorAll('.heading-1, .heading-2');
+
+        for (let i: number = 0; i < headingElements.length; i++) {
+            let headingElement = headingElements[i] as HTMLElement;
+            let buttonElement = headingElement.querySelector('div[role="button"]') as HTMLElement;
+
+            let headingID = headingElement.getAttribute('id');
+            let href = `${location.protocol}//${location.host}${location.pathname}${headingID}`;
+
+            // Clipboard for button
+            buttonElement.setAttribute('data-clipboard-text', href);
+            new Clipboard(buttonElement);
+
+            // 'Popup' for button
+            this._tooltipService.setupElement(buttonElement, 'right');
+        }
+    }
+}
