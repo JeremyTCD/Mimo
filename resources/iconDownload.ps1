784584ae8bf5e40f2b2ca1736e55153e4bdfe059
JeremyTCD <jeremytanchongdao@gmail.com>
2018-10-06 12:38:21 +0800

    Minor changes to resources

--- resources/iconDownload.ps1
+++ resources/iconDownload.ps1
@@ -1,39 +1,39 @@
 # Simple script for downloading material design icons
 
-$materialIconNames = @(
-	"file_copy",
-	"arrow_upward",
-	"chevron_right",
-	"clear",
-	"copyright",
-	"date_range",
-	"arrow_drop_down",
-	"filter",
-	"info",
-	"link",
-	"edit",
-	"navigate_next",
-	"person",
-	"navigate_before",
-	"search",
-	"share",
-	"warning"
+$materialIcons = @(
+	@{name="arrow_upward"; style="baseline"},
+	@{name="arrow_drop_down"; style="baseline"},
+	@{name="file_copy"; style="baseline"},
+	@{name="link"; style="baseline"},
+	@{name="info"; style="baseline"},
+	@{name="warning"; style="baseline"},
+	@{name="navigate_next"; style="baseline"},
+	@{name="navigate_before"; style="baseline"},
+	@{name="date_range"; style="sharp"},
+	@{name="person"; style="baseline"},
+	@{name="filter_list"; style="baseline"},
+	@{name="clear"; style="baseline"},
+	@{name="search"; style="baseline"},
+	@{name="share"; style="baseline"},
+	@{name="edit"; style="baseline"},
+	@{name="chevron_right"; style="baseline"},
+	@{name="copyright"; style="baseline"}
 )
 
-$materialUrlFormat = "https://material.io/tools/icons/static/icons/sharp-{0}-24px.svg"
+$materialUrlFormat = "https://material.io/tools/icons/static/icons/{0}-{1}-24px.svg"
 $outfileFormat = "material-design-{0}.svg"
 $requireFormat = "require('../../resources/{0}')"
 
 $requires = ""
 
-foreach($materialIconName in $materialIconNames){
-	$normalizedMaterialIconName = $materialIconName.replace('_', '-')
+foreach($materialIcon in $materialIcons){
+	$normalizedMaterialIconName = $materialIcon.name.replace('_', '-')
 	$outfile = $outfileFormat -f $normalizedMaterialIconName
-	$url = $materialUrlFormat -f $materialIconName
+	$url = $materialUrlFormat -f $materialIcon.style, $materialIcon.name
 
-	"Downloading: " + $materialIconName + " From: " + $url + " To: " + $outfile
+	"Downloading: " + $materialIcon.name + " From: " + $url + " To: " + $outfile
 
-	wget $url -outfile $outfile
+	Invoke-WebRequest $url -outfile $outfile
 
 	$requires = $requires + "`n" + ($requireFormat -f $outfile)
 }
