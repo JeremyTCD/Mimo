2fc47f5d2fa92bc41341cf1eb725d5beb06a4292
JeremyTCD <jeremytanchongdao@gmail.com>
2018-10-08 16:00:49 +0800

    Fixed spelling mistake in pathService.ts

--- scripts/shared/pathService.ts
+++ scripts/shared/pathService.ts
@@ -5,7 +5,7 @@ export default class PathService {
     public getAbsolutePath(href: string): string {
         // Use anchor to normalize href
         let anchor = $('<a href="' + href + '"></a>')[0] as HTMLAnchorElement;
-        // Ignore protocal, remove search and query
+        // Ignore protocol, remove search and query
         return anchor.host + anchor.pathname;
     }
 
