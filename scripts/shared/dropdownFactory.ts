3b369b51a3861bccb20764c273e51c73cdace7d4
JeremyTCD <jeremytanchongdao@gmail.com>
2019-02-20 22:02:17 +0800

    Made improvements to dropdown.ts
    - Simplified required markup.
    - Added callbacks.

--- scripts/shared/dropdownFactory.ts
+++ scripts/shared/dropdownFactory.ts
@@ -1,14 +1,28 @@
 ﻿import { injectable } from "inversify";
 import Dropdown from './dropdown';
+import OverlayService from "./overlayService";
 
 @injectable()
 export default class DropdownService {
-    public build(
-        contentOuterWrapperElement: HTMLElement,
-        contentInnerWrapperElement: HTMLElement,
-        toggleElement: HTMLElement,
-        headerAndContentWrapperElement: HTMLElement) {
+    public constructor(private _overlayService: OverlayService) {
+    }
 
-        return new Dropdown(contentOuterWrapperElement, contentInnerWrapperElement, toggleElement, headerAndContentWrapperElement);
+    public build(rootElement: HTMLElement,
+        onExpand?: () => void,
+        onCollapse?: () => void,
+        onDropdownButtonClick?: () => void,
+        defaultTranslateToTop = true,
+        defaultAnimate = true,
+        defaultDisableBodyScroll = true,
+        defaultLimitDropdownBodyMaxHeight = true) {
+        return new Dropdown(rootElement,
+            this._overlayService,
+            onExpand,
+            onCollapse,
+            onDropdownButtonClick,
+            defaultTranslateToTop,
+            defaultAnimate,
+            defaultDisableBodyScroll,
+            defaultLimitDropdownBodyMaxHeight);
     }
 }
