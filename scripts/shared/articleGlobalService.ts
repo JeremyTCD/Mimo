3b74a5c4179fffdfe00903c4c76093eedf071e71
JeremyTCD <jeremytanchongdao@gmail.com>
2019-03-02 15:40:51 +0800

    Made minor changes to articleGlobalService.ts.

--- scripts/shared/articleGlobalService.ts
+++ scripts/shared/articleGlobalService.ts
@@ -1,9 +1,8 @@
 ﻿import { named, injectable, inject } from 'inversify';
 import MediaGlobalService from './mediaGlobalService';
-import DebounceService from './debounceService';
 import GlobalService from './globalService';
-import * as SmoothScroll from 'smooth-scroll';
 import { MediaWidth } from './mediaWidth';
+var SmoothScroll = require('./smoothScroll');
 
 interface ObservedSectionData {
     element: HTMLElement;
@@ -15,31 +14,22 @@ interface ObservedSectionData {
 
 @injectable()
 export default class ArticleGlobalService implements GlobalService {
-    private _mediaGlobalService: MediaGlobalService;
-    private _debounceService: DebounceService;
-
-    private _noOutline: boolean;
+    private _onlyOneSection: boolean;
     private _sectionElements: HTMLElement[];
-    private _observedSectionDatas: ObservedSectionData[];
-    private _sectionMarginTops: number[]; // For checking whether sections can be scrolled to (not too close to end of body)
-    private _updateHistoryDebounced: () => void;
-    private _sectionHashes: string[]; // For updating location.hash (updates url in search bar)
+    private _observedSectionDatas: ObservedSectionData[] = [];
+    private _sectionHashes: string[] = []; // For updating location.hash (updates url in search bar)
     private _activeSectionIndexFixed: boolean;
-    private _activeSectionChangedListeners: ((newIndex: number) => void)[];
-    private _activeSectionIndex: number;
+    private _activeSectionChangedListeners: ((newIndex: number) => void)[] = [];
+    private _activeSectionIndex: number = 0;
     private _narrowIntersectionObserver: IntersectionObserver; // There is a permanent header when mode is narrow, so root margin must be set
     private _mediumWideIntersectionObserver: IntersectionObserver;
-    private _smoothScroll: SmoothScroll;
-    private _headerHeight: number;
+    private _smoothScrollOffset: number = 0; // If onlyOneSection is true, _smoothScrollOffset is still used by tryGetActiveSectionIndexFromHash
     private _headerEnabled: boolean;
     private _narrowInterSectionListenerCorrected: boolean;
+    private _level2SectionSmoothScrollOffset: number;
 
     public constructor(
-        @inject('GlobalService') @named('MediaGlobalService') mediaGlobalService: MediaGlobalService,
-        debounceService: DebounceService) {
-        this._mediaGlobalService = mediaGlobalService;
-        this._debounceService = debounceService;
-        this._activeSectionIndex = 0; // Should never be undefined
+        @inject('GlobalService') @named('MediaGlobalService') private _mediaGlobalService: MediaGlobalService) {
     }
 
     public setupOnDomContentLoaded(): void {
@@ -47,24 +37,29 @@ export default class ArticleGlobalService implements GlobalService {
     }
 
     public setupImmediate(): void {
-        // Setup smooth scroll
-        this.setupSmoothScroll();
+        // Check if article menu header is enabled
+        this._headerEnabled = !!document.querySelector('.article-menu');
 
-        // Check if header is enabled
-        this._headerEnabled = !!document.getElementById('article-menu-header');
+        // Smooth scroll
+        SmoothScroll('a[href*="#"]', {
+            speed: 150,
+            speedAsDuration: true,
+            offset: (anchor: HTMLElement, _: HTMLElement) => {
+                if (!this._mediaGlobalService.mediaWidthIs(MediaWidth.narrow)) {
+                    return 0;
+                }
+                return anchor.classList.contains('flexi-section-block-2') ? this._level2SectionSmoothScrollOffset : this._smoothScrollOffset;
+            }
+        });
 
         // Setup section datas for observation
         this._sectionElements = Array.from<HTMLElement>(document.querySelectorAll('.main-article, .main-article > .flexi-section-block-2, .main-article > .flexi-section-block-2 > .flexi-section-block-3')); // Ignore sections in subtrees
 
-        if (this._sectionElements.length == 1) { // Only article
-            this._noOutline = true;
+        this._onlyOneSection = this._sectionElements.length === 1; // Only article
+        if (this._onlyOneSection) {
             return;
-        } else {
-            this._noOutline = false;
         }
 
-        this._observedSectionDatas = [];
-        this._sectionHashes = [];
         this._sectionElements.forEach((sectionElement: Element, index: number) => {
             let level: number = 3;
             if (sectionElement.classList.contains('main-article')) {
@@ -85,19 +80,12 @@ export default class ArticleGlobalService implements GlobalService {
             // Set section hashes
             this._sectionHashes[index] = `#${sectionElement.id}`;
         });
-
-        // Misc setup
-        this._sectionMarginTops = [];
-        this._activeSectionChangedListeners = [];
-        this._updateHistoryDebounced = this._debounceService.createTimeoutDebounceFunction(this.updateHistory, 100);
     }
 
     public setupOnLoad(): void {
-        if (this._noOutline) {
+        if (this._onlyOneSection) {
             return;
         }
-
-        this.setupSectionMarginTops();
 
         this._mediaGlobalService.addChangedToListener(this.onChangedToNarrowListener, MediaWidth.narrow)
         this._mediaGlobalService.addChangedFromListener(this.onChangedFromNarrowListener, MediaWidth.narrow);
@@ -107,19 +95,10 @@ export default class ArticleGlobalService implements GlobalService {
         document.addEventListener('scrollStop', this.smoothScrollAfter, false);
 
         // Set initial _activeSectionIndex
-        let hash = location.hash;
-
-        if (hash && hash !== '#top' && hash !== '#') {
-            let hashContent = hash.substring(1);
-            let section = this._sectionElements.find(element => element.id === hashContent);
-
-            if (section) {
-                this._smoothScroll.animateScroll(section); // When article menu header is visible (narrow), if user navigated to #<some element>, the initial page load does not account for header height.
-                return;
-            }
+        let activeSectionIndex = this.tryGetActiveSectionIndexFromHash();
+        if (activeSectionIndex > -1) {
+            this.setActiveSectionIndex(activeSectionIndex);
         }
-
-        this.setActiveSectionIndex(0); // Call even though _activeSectionIndex is 0 by default so that listeners get called
     }
 
     private onChangedToNarrowListener = (): void => {
@@ -127,13 +106,8 @@ export default class ArticleGlobalService implements GlobalService {
             this._mediumWideIntersectionObserver.disconnect();
         }
 
-        // Set header height
-        if (this._headerEnabled) {
-            this._headerHeight = 37;
-        }
-        else {
-            this._headerHeight = 0;
-        }
+        this._smoothScrollOffset = this._headerEnabled ? 37 : 0;
+        this._level2SectionSmoothScrollOffset = this._headerEnabled ? 18 : -19; // Limit top gap on mobiles
 
         // Create and register intersection observer
         if (!this._narrowIntersectionObserver) {
@@ -144,7 +118,7 @@ export default class ArticleGlobalService implements GlobalService {
     }
 
     private createNarrowIntersectionObserver = (multiplyByDevicePixelRatio: boolean = false): IntersectionObserver => {
-        let topMargin = -this._headerHeight;
+        let topMargin = -this._smoothScrollOffset;
         if (multiplyByDevicePixelRatio) {
             topMargin *= window.devicePixelRatio;
         }
@@ -157,8 +131,7 @@ export default class ArticleGlobalService implements GlobalService {
             this._narrowIntersectionObserver.disconnect();
         }
 
-        // Set header height
-        this._headerHeight = 0;
+        this._level2SectionSmoothScrollOffset = this._smoothScrollOffset = 0;
 
         // Create and register intersection observer
         if (!this._mediumWideIntersectionObserver) {
@@ -179,15 +152,15 @@ export default class ArticleGlobalService implements GlobalService {
     }
 
     private onIntersectionListener = (entries: IntersectionObserverEntry[], _: IntersectionObserver) => {
-
         // On Chrome for Android, rootMargins are read as device pixels (as opposed to CSS pixels > window.devicePixelRatio = devicePixels/cssPixels). 
         // To verify whether or not we are dealing with Chrome for Android, we check whether the computed rootBounds.top is equal to what we requested, _headerHeight.
         // If it isn't, we ditch the initial _narrowIntersectionObserver and create a new one with rootMargins multiplied by window.devicePixelRatio.
-        if (this._headerHeight != 0 && // We don't use rootMargins when headerHeight is 0
-            !this._narrowInterSectionListenerCorrected && // Only correct once. Chrome for Anrdoid computes rootBounds.top as ourSpecifiedRootMarginTop/window.devicePixelRatio. Even
-                                                          // if we specify outSpecifiedRootMarginTop as _headerHeight * window.devicePixelRatio, rootBounds.top may not === _headerHeight
-                                                          // due to the accuracy limitations of floating point calculations.
-            entries[0].rootBounds.top !== this._headerHeight) {
+        if (!this._narrowInterSectionListenerCorrected && // Only correct once. Why? Chrome for Android computes rootBounds.top as ourSpecifiedRootMarginTop/window.devicePixelRatio. Even
+            // if we specify ourSpecifiedRootMarginTop as _smoothScrollOffset * window.devicePixelRatio, rootBounds.top may not === _smoothScrollOffset
+            // due to the accuracy limitations of floating point calculations.
+            this._smoothScrollOffset !== 0 && // If offset is 0, nothing to correct
+            this._mediaGlobalService.mediaWidthIs(MediaWidth.narrow) && // In medium and wide mode, rootBounds.top is always 0
+            entries[0].rootBounds.top !== this._smoothScrollOffset) {
             this._narrowInterSectionListenerCorrected = true;
             this._narrowIntersectionObserver.disconnect();
             this._narrowIntersectionObserver = this.createNarrowIntersectionObserver(true);
@@ -202,12 +175,12 @@ export default class ArticleGlobalService implements GlobalService {
 
             this._observedSectionDatas.some((observedSectionData: ObservedSectionData) => {
                 // Since threshold is 0, if callback has fired for element, isIntersecting == isVisible.
-                if (observedSectionData.element == entry.target) {
+                if (observedSectionData.element === entry.target) {
                     observedSectionData.isVisible = entry.isIntersecting;
 
                     return true;
                 }
-                else if (observedSectionData.prevNonSectionElement == entry.target) {
+                else if (observedSectionData.prevNonSectionElement === entry.target) {
                     observedSectionData.prevIsVisible = entry.isIntersecting;
 
                     return true;
@@ -238,13 +211,17 @@ export default class ArticleGlobalService implements GlobalService {
             return false;
         });
 
+        if (activeSectionIndex === -1) { // No sections visible yet (could happen if viewport height is small)
+            activeSectionIndex = 0;
+        }
+
         if (this.setActiveSectionIndex(activeSectionIndex)) {
-            this._updateHistoryDebounced();
+            this.updateHistory();
         }
     }
 
     public addActiveSectionChangedListener(listener: (newIndex: number) => void, init: boolean = false) {
-        if (this._noOutline) {
+        if (this._onlyOneSection) {
             return;
         }
 
@@ -256,7 +233,7 @@ export default class ArticleGlobalService implements GlobalService {
     }
 
     public removeActiveSectionChangedListener(listener: (newIndex: number) => void) {
-        if (this._noOutline) {
+        if (this._onlyOneSection) {
             return;
         }
 
@@ -267,13 +244,7 @@ export default class ArticleGlobalService implements GlobalService {
         }
     }
 
-    private setupSectionMarginTops() {
-        for (let i = 0; i < this._observedSectionDatas.length; i++) {
-            this._sectionMarginTops.push(parseFloat(getComputedStyle(this._observedSectionDatas[i].element).marginTop));
-        }
-    }
-
-    public getActiveHeaderIndex(): number {
+    public getActiveSectionIndex(): number {
         return this._activeSectionIndex;
     }
 
@@ -281,47 +252,32 @@ export default class ArticleGlobalService implements GlobalService {
         return this._sectionElements;
     }
 
-    private setupSmoothScroll(): void {
-        this._smoothScroll = new SmoothScroll('a[href*="#"]', {
-            speed: 300,
-            header: '#article-menu-header' // Smooth-scroll updates header height when page is resized (this was removed in later versions of smooth-scroll, be careful when updating)
-        });
-    }
-
-    // Chrome and edge have rendering issues if activeHeaderIndex changes rapidly and knob translate/scale are updated rapidly.
-    // Fixing activeHeaderIndex on smooth-scroll avoids these rendering issues.
-    private smoothScrollBefore = (): void => {
+    private tryGetActiveSectionIndexFromHash(): number {
         let hash = location.hash;
-
-        this._activeSectionIndexFixed = true;
 
         // # or #top navigate to the top of the page - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#attr-href.
         // Smooth-scroll used to add a #smooth-scroll-top id to body to scroll to the top, it no longer does that, instead
         // it just scrolls to # or #top - https://github.com/cferdinandi/smooth-scroll/commit/37f579b05f6173bba300777867f6b8b613339662#diff-2fa6e0fecc1866964986277037867a1cR468.
         if (!hash || hash === '#top' || hash === '#') {
-            this.setActiveSectionIndex(0);
-            return;
+            return 0;
         }
 
-        let bodyBottom = document.body.getBoundingClientRect().bottom;
+        let bodyHeight = document.body.getBoundingClientRect().height; // May change so we have to retrieve it each time
         for (let i = 0; i < this._sectionHashes.length; i++) {
             if (hash === this._sectionHashes[i]) {
                 // Ensure that section can be scrolled to
-                // Note that BoundingClientRect.top does not include top margin.
-                while (bodyBottom - this._observedSectionDatas[i].element.getBoundingClientRect().top + this._sectionMarginTops[i] < window.innerHeight - this._headerHeight) {
+                while (bodyHeight - this._observedSectionDatas[i].element.offsetTop < window.innerHeight - this._smoothScrollOffset) { // offsetTop may change, so it can't be cached
                     if (i === 0) { // If we get to 0, it means that even the first section can't be scrolled to
                         break;
                     }
                     i--;
                 }
 
-                this.setActiveSectionIndex(i);
-                return;
+                return i;
             }
         }
 
-        // Not navigating to top or an article section (could be a link in the article to another element in the article)
-        this._activeSectionIndexFixed = false;
+        return -1;
     }
 
     private setActiveSectionIndex(newIndex: number): boolean {
@@ -336,6 +292,20 @@ export default class ArticleGlobalService implements GlobalService {
         }
 
         return true;
+    }
+
+    // Chrome and edge have rendering issues if activeHeaderIndex changes rapidly and knob translate/scale are updated rapidly.
+    // Fixing activeHeaderIndex on smooth-scroll avoids these rendering issues.
+    private smoothScrollBefore = (): void => {
+        this._activeSectionIndexFixed = true;
+        let endActiveSectionIndex = this.tryGetActiveSectionIndexFromHash();
+
+        if (endActiveSectionIndex > -1) {
+            this.setActiveSectionIndex(endActiveSectionIndex);
+        } else {
+            // Not navigating to top or an article section (could be a link in the article to another element in the article)
+            this._activeSectionIndexFixed = false;
+        }
     }
 
     private smoothScrollAfter = (): void => {
