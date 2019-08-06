1d9a7de71ba57cc3d95d2b2da036dee77d1f0b1b
JeremyTCD <jeremytanchongdao@gmail.com>
2019-08-06 21:46:09 +0800

    Fixed mistake in codeService.ts
    - Properly handles copying of code for code blocks with line numbers.

--- scripts/shared/codeService.ts
+++ scripts/shared/codeService.ts
@@ -11,7 +11,7 @@ export default class CodeService {
     }
 
     public setupCodeBlocks() {
-        let codeBlockElements = document.querySelectorAll(".flexi-code-block");
+        let codeBlockElements = document.querySelectorAll(".flexi-code");
 
         // On click "popups"
         for (let i = 0; i < codeBlockElements.length; i++) {
@@ -27,16 +27,34 @@ export default class CodeService {
             this._tooltipService.setupElement(copyButtonElement, 'left', 'Code copied');
 
             // Setup copying to clipboard
+            let textGenerator: () => string;
             let code = null;
-            new Clipboard(copyButtonElement, {
-                text: function () {
+            if (codeBlockElement.classList.contains("flexi-code_no-line-numbers")) {
+                textGenerator = function () {
                     // Do this lazily - not likely that all code blocks will be copied.
                     if (code === null) {
                         code = codeElement.innerText;
                     }
                     return code;
-                },
+                };
+            } else {
+                textGenerator = function () {
+                    if (code === null) {
+                        code = '';
+                        let lineElements = codeElement.querySelectorAll(".flexi-code__line:not(.flexi-code__line_omitted-lines)");
+                        let lineElementsLastIndex = lineElements.length - 1;
+                        lineElements.forEach((value: Element, index: number) => {
+                            code += (value as HTMLSpanElement).innerText + (index == lineElementsLastIndex ? '' : '\n');
+                        });
+                    }
+                    return code;
+                }
+            }
+
+            new Clipboard(copyButtonElement, {
+                text: textGenerator
             });
         }
     }
 }
+
